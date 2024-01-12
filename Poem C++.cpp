e64a176dd5ecc3fa0d74b8069211529ef98f9aef
SchaimardanovRenat <145016510+SchaimardanovRenat@users.noreply.github.com>
2024-01-12 11:36:06 +0300

    Add files via upload

---
+++ Poem C++.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "Любить вас долго было б скучно,\n";
+	cout << "Любить до гроба — право, смех,\n";
+	cout << "Пройти ж вас мимо равнодушно - \n";
+	cout << "Перед собою тяжкий грех \n";
+	return 0;
+
+}
